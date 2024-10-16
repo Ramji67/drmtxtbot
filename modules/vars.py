e9365e8e232b900e4197ba71a3572f1360d09d22
Ramji67 <158837797+Ramji67@users.noreply.github.com>
2024-10-16 21:41:46 +0530

    vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,5 +1,5 @@
 api_id = 28088290
 api_hash = "6998f2c585fdce65ac72dfa23d02b6ec"
 bot_token = "7452214279:AAHakbBdOuczYkVhwloCDySbdO0LqemFRYk"
-AUTH = 6830450483
+AUTH = 5850397219
 #Bot Username = "Upload_tst_bot"
